721b26ebd49c127979da947f86cd85acf255bafc
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-10-21 15:55:48 +0530

    Modified for post creation

--- src/PostWrapper.jsx
+++ src/PostWrapper.jsx
@@ -3,17 +3,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Modal, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import Posts from './Posts';
-import AddForm from './AddForm';
+import ModalForm from './ModalForm';
 import { getPosts } from './actions/postActions';
-//import { GET_POSTS } from './actions/types';
 
 function PostWrapper(props) {
 	const dispatch = useDispatch();
 	const [show, setShow] = useState(false);
+	const [create, setCreate] = useState(true);
 	const { posts } = useSelector((state) => state.post);
-	// console.log(posts);
 
 	const handleShow = () => setShow(true);
 	const handleClose = () => setShow(false);
@@ -21,18 +20,19 @@ function PostWrapper(props) {
 	useEffect(() => {
 		const loadPosts = async () => {
 			const resp = await axios.get('https://jsonplaceholder.typicode.com/posts');
-			//setPosts(response.data);
+
 			dispatch(getPosts(resp.data));
-			//console.log(resp.data);
-			//dispatch({ type: GET_POSTS, payload: resp.data });
 		};
 
 		loadPosts();
 	}, []);
 
-	useEffect(() => {
-		handleClose();
-	}, [posts]);
+	const title = '';
+	const body = '';
+	const post = {
+		title,
+		body
+	};
 
 	return (
 		<div className='App'>
@@ -43,19 +43,8 @@ function PostWrapper(props) {
 					<Button variant='primary' onClick={handleShow} data-toggle='modal'>
 						Create Post
 					</Button>
-					<Modal show={show} onHide={handleClose}>
-						<Modal.Header closeButton>
-							<Modal.Title>Create Post</Modal.Title>
-						</Modal.Header>
-						<Modal.Body>
-							<AddForm />
-						</Modal.Body>
-						<Modal.Footer>
-							<Button variant='secondary' onClick={handleClose}>
-								Close
-							</Button>
-						</Modal.Footer>
-					</Modal>
+
+					<ModalForm show={show} close={handleClose} isCreate={create} post={post} />
 				</div>
 			</div>
 			<div className='Wrapper'>
