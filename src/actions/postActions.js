8fe77e3f538ce46c0d11477ac42a3734c624afd0
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-10-21 14:34:02 +0530

    Modified to import all action types

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,38 +1,29 @@
-import { GET_POSTS, CREATE_POST, EDIT_POST, DELETE_POST } from './types';
+import * as actions from './types';
 
 export const getPosts = (post) => {
 	return {
-		type: GET_POSTS,
+		type: actions.GET_POSTS,
 		payload: post
 	};
 };
 
 export const createPost = (post) => {
 	return {
-		type: CREATE_POST,
+		type: actions.CREATE_POST,
 		payload: post
 	};
 };
 
-export const editPost = (index, post) => {
+export const updatePost = (post) => {
 	return {
-		type: EDIT_POST,
-		index,
+		type: actions.UPDATE_POST,
 		payload: post
 	};
 };
-
-// export const updatePost = (post, index) => {
-// 	return {
-// 		type: UPDATE_POST,
-// 		post,
-// 		index
-// 	};
-// };
 
 export const deletePost = (index) => {
 	return {
-		type: DELETE_POST,
+		type: actions.DELETE_POST,
 		payload: index
 	};
 };
