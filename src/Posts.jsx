abce3ec79f2fb22dfdfb20974b802a660bc0b0dd
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-10-21 15:57:25 +0530

    Modified to use Modal component for Post updation

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getPosts, createPost, editPost, deletePost } from './actions/postActions';
+
+import { deletePost } from './actions/postActions';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
+import ModalForm from './ModalForm';
 
 const Posts = () => {
-	const postsList = useSelector((state) => state.posts.posts);
+	const postsList = useSelector((state) => state.post.posts);
+	//console.log(postsList);
 	let dispatch = useDispatch();
-	//console.log(`Fetched data using useSelector: ${postsList}`);
+	const [show, setShow] = useState(false);
+	const [create, setCreate] = useState(false);
+	const [post, setPost] = useState();
+
+	const handleShow = (post) => {
+		setPost(post);
+		setShow(true);
+	};
+	const handleClose = () => setShow(false);
 
 	return (
 		<>
-			{postsList.map((post) => (
-				<div className='Post' key={post.title}>
-					<img src={`https://picsum.photos/300/300?random&t=${post.id}`} alt='random photos' />
-					<h1>{post.title}</h1>
-					<p>{post.body}</p>
-					{/* <EditModal key={post.id} postId={post.id} /> */}
-					<div className='control-buttons'>
-						<Button variant='outline-info'>Edit</Button>
-						<Button variant='outline-danger' onClick={() => dispatch(deletePost(post.id))}>
-							Delete
-						</Button>
+			{postsList &&
+				postsList.map((post) => (
+					<div className='Post' key={post.title}>
+						<div className='post-body'>
+							<img src={`https://picsum.photos/300/300?random&t=${post.id}`} alt='random photos' />
+							<h1>{post.title}</h1>
+							<p>{post.body}</p>
+						</div>
+						<div className='control-buttons'>
+							<Button variant='outline-info' onClick={() => handleShow(post)}>
+								Edit
+							</Button>
+							<Button variant='outline-danger' onClick={() => dispatch(deletePost(post.id))}>
+								Delete
+							</Button>
+						</div>
 					</div>
-				</div>
-			))}
+				))}
+
+			<ModalForm show={show} close={handleClose} isCreate={create} post={post} />
 		</>
 	);
 };
