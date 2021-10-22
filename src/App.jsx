d4c2081d549e529830c226d5ba7ffc4bbae724ce
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-10-22 23:06:25 +0530

    Added routes for custom hook and HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import PostWrapper from './PostWrapper';
+import Navbar from './Navbar';
+import Custom from './Custom';
+import HOC from './hoc/hoc';
 
 function App() {
 	return (
-		<div>
-			<PostWrapper postTitle='My Posts' pageTitle='My Post App' />;
-		</div>
+		<main>
+			<Navbar />
+			<Switch>
+				<Route path='/custom'>
+					<Custom />
+				</Route>
+				<Route path='/hoc'>
+					<HOC />
+				</Route>
+				<Route path='/'>
+					<PostWrapper postTitle='My Posts' pageTitle='My Post App' />;
+				</Route>
+			</Switch>
+		</main>
 	);
 }
+
+// function Home() {
+// 	return <h2>Home</h2>;
+//   }
 
 export default App;
