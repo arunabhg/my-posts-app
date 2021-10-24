ebeb356119840705dfb78705b61279d1f9557978
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-10-24 14:35:09 +0530

    Added public and private route for login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 import PostWrapper from './PostWrapper';
 import Navbar from './Navbar';
 import Custom from './Custom';
 import HOC from './hoc/hoc';
+import LoginForm from './Login';
+import Dashboard from './Dashboard';
+import PrivateRoute from './utils/PrivateRoute';
+import PublicRoute from './utils/PublicRoute';
+import { getToken, removeUserSession, setUserSession } from './utils/Common';
 
 function App() {
+	const [authLoading, setAuthLoading] = useState(true);
+
+	useEffect(() => {
+		const token = getToken();
+		if (!token) {
+			return;
+		}
+
+		axios
+			.get(`http://localhost:4000/verifyToken?token=${token}`)
+			.then((response) => {
+				setUserSession(response.data.token, response.data.user);
+				setAuthLoading(false);
+			})
+			.catch((error) => {
+				removeUserSession();
+				setAuthLoading(false);
+			});
+	}, []);
+
+	if (authLoading && getToken()) {
+		return <div className='content'>Checking Authentication...</div>;
+	}
+
 	return (
 		<main>
 			<Navbar />
 			<Switch>
+				<PublicRoute path='/login' component={LoginForm} />
+				<PrivateRoute path='/dashboard' component={Dashboard} />
 				<Route path='/custom'>
 					<Custom />
 				</Route>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Navbar() {
+	const [show, setShow] = useState(false);
+
 	return (
 		<header className='navMenu'>
 			<NavLink exact to='/' className='navLink' activeClassName='activeRoute'>
@@ -12,6 +14,12 @@ function Navbar() {
 			</NavLink>
 			<NavLink to='/hoc' className='navLink' activeClassName='activeRoute'>
 				HOC
+			</NavLink>
+			<NavLink to='/dashboard' className='navLink' activeClassName='activeRoute'>
+				Dashboard
+			</NavLink>
+			<NavLink to='/login' className='navLink'>
+				<button onClick={() => setShow(true)}>Login</button>
 			</NavLink>
 		</header>
 	);
